07975fc8738b43d7110339fe218493d1f5543374
kRahul123 <k003.rahul@gmail.com>
2020-07-12 16:16:08 +0530

    basic template models for player and employee

---
+++ game/src/main/java/com/psych/game/model/Employee.java
@@ -0,0 +1,26 @@
+package com.psych.game.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="employees")
+public class Employee extends Auditable {
+
+
+    @Getter
+    @Setter
+    @NotBlank
+    private String name;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Email
+    private String email;
+
+}
---
+++ game/src/main/java/com/psych/game/model/Player.java
@@ -0,0 +1,23 @@
+package com.psych.game.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name="players")
+public class Player extends Auditable {
+
+
+    @Getter
+    @Setter
+    @NotBlank
+    private String name;
+
+
+}
